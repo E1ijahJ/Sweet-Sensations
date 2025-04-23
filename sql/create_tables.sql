d1ac3ffb8e59e8b7fa58919d8871721ff600d860
Jaylen Santiago <jsan6767@students.vsu.edu>
2025-04-23 11:17:00 -0400

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -55,13 +55,11 @@ CREATE TABLE Order_Items (
 );
 
 -- Create Cart table
-Create Table Cart(
+CREATE TABLE Cart (
     Cart_id INT PRIMARY KEY,
-    cart_id INT,
     product_id INT,
     quantity INT,
-    FOREIGN KEY (cart_id) REFERENCES carts(id),
-    FOREIGN KEY (product_id) REFERENCES products(id) 
+    FOREIGN KEY (product_id) REFERENCES Products(ProductID)
 );
 
 
